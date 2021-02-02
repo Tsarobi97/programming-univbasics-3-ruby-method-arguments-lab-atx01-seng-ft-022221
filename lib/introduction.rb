8f9498ac3af1f1240bb13534635f735834c92697
Robert Obi <robobi97@googlemail.com>
2021-02-02 21:21:03 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,16 +2,19 @@ def introduction(name)
   puts "Hi, my name is #{name}"
 end 
 
+introduction("Josh")
+
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end 
+
+introduction_with_language("Dan", "Ember.js")
+
 
 def introduction_with_language_option(name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learing to program in #{language}"
 end 
 
-introduction("Josh.")
-introduction_with_language("Dan", "Ember.js")
 introduction_with_language_option("Edwin", "Python")
 
 
